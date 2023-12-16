88d6e820613e34c2762b7bac0c5073cd6c4e9738
latterr <98669157+latterr@users.noreply.github.com>
2023-12-16 15:34:22 +0300

    Deleted 2 useless scripts from the Compiled Build

--- Compiled Build/mods/scripts/cameraFix.hx
+++
@@ -1,7 +0,0 @@
-function onCreatePost(){
-	var G = FlxG.state;
-	//Instantly snap camera to correct position (GF's camera position in this case)
-	G.camFollowPos.setPosition(G.camFollow.x,G.camFollow.y);
-	
-	//Reminder, this is 0.6.3, camera stuff is different from 0.7.X !!!!
-}

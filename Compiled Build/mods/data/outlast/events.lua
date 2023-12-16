22047c3a294661c244744ab556230736f4e508cd
latterr <98669157+latterr@users.noreply.github.com>
2023-12-16 16:31:19 +0300

    Add outlast events, add music to the dialogue

---
+++ Compiled Build/mods/data/outlast/events.lua
@@ -0,0 +1,97 @@
+function onCreate()
+addCharacterToList('creature', 'dad')
+addCharacterToList('AM', 'dad')
+end
+
+function onCreatePost()
+setProperty('camGame.alpha', 0)
+setProperty('camHUD.alpha', 0)
+setProperty('boyfriend.alpha', 0)
+end
+
+function onSongStart()
+setProperty('camGame.alpha', 1)
+doTweenAlpha('1', 'camGame', 0,1.5)
+end
+
+
+function onStepHit()
+if curStep == 32 or curStep == 64 or curStep == 96 or curStep == 736 then
+doTweenAlpha('1', 'camGame', 0,1.5)
+setProperty('camGame.alpha', 1)
+elseif curStep == 30 or curStep == 94 then
+setProperty('boyfriend.alpha', 1)
+setProperty('dad.alpha', 0)
+elseif curStep == 62 then
+setProperty('boyfriend.alpha', 0)
+setProperty('dad.alpha', 1)
+elseif curStep == 126 then
+setProperty('dad.alpha', 1)
+elseif curStep == 128 then
+doTweenAlpha('1', 'camGame', 1,1)
+doTweenAlpha('2', 'camHUD', 1,1)
+
+elseif curStep == 1040 then
+setProperty('boyfriend.x', 700)
+setProperty('boyfriend.y', 350)
+setProperty('dad.x', 0)
+setProperty('dad.y', 0)
+setProperty('bg.visible', false)
+setProperty('iconP1.visible', false)
+setProperty('healthBar.visible', false)
+setProperty('healthBarBG.visible', false)
+setProperty('iconP2.visible', false)
+setProperty('scoreTxt.visible', false)
+setProperty('timeBar.visible', false)
+setProperty('timeBarBG.visible', false)
+setProperty('timeTxt.visible', false)
+
+elseif curStep == 1088 then
+doTweenAlpha('1', 'camGame', 1, 1.5)
+elseif curStep == 1152 then
+doTweenAlpha('2', 'camHUD', 1, 1.5)
+elseif curStep == 1280 then
+setProperty('boyfriend.x', 2000)
+setProperty('boyfriend.y', 600 + 350)
+setProperty('dad.x', -240 + 1200)
+setProperty('dad.y', -445 + 580)
+setProperty('boyfriend.alpha', 1)
+elseif curStep == 1730 then
+setProperty('dad.x', 0)
+setProperty('dad.y', 0)
+setProperty('bg.visible', false)
+setProperty('boyfriend.alpha', 0)
+elseif curStep == 1736 then
+doTweenAlpha('1', 'camGame', 1, 10)
+elseif curStep == 1856 then
+doTweenAlpha('1', 'camGame', 0, 0.8)
+end
+end
+
+function onEvent(n,v1,v2)
+if n == 'camera_target' then
+if v1 == 'all' then
+folowcam = true
+else
+ folowcam = false
+end
+end
+
+if n == 'WBG' then
+if v1 == 'Bon' or v1 == 'Won' then
+setProperty('bg.visible', false)
+else
+ setProperty('bg.visible', true)
+end
+end
+end
+
+folowcam = false
+camX = 1900
+camY = 900
+function onUpdate()
+if folowcam then
+setProperty('camFollow.x', camX)
+setProperty('camFollow.y', camY)
+end
+end
--- Compiled Build/mods/data/outlast/playDialogue.lua
+++ Compiled Build/mods/data/outlast/playDialogue.lua
@@ -1,20 +1,24 @@
+function onCreate()
+precacheMusic('pause_busridehome')
+end
+
 local allowCountdown = false
 function onStartCountdown()
-	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
-	if not allowCountdown and isStoryMode and not seenCutscene then
-		setProperty('inCutscene', true);
-		runTimer('startDialogue', 0.8);
-		allowCountdown = true;
-		return Function_Stop;
-	end
-    
-	return Function_Continue;
+-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
+if not allowCountdown and isStoryMode and not seenCutscene then
+setProperty('inCutscene', true);
+runTimer('startDialogue', 0.8);
+allowCountdown = true;
+return Function_Stop;
+end
+
+return Function_Continue;
 end
 
 function onTimerCompleted(tag, loops, loopsLeft)
-	if tag == 'startDialogue' then -- Timer completed, play dialogue
-        addHaxeLibrary("DialogueUtil","tools")
-        runHaxeCode([[
+if tag == 'startDialogue' then -- Timer completed, play dialogue
+addHaxeLibrary("DialogueUtil","tools")
+runHaxeCode([[
             DialogueUtil.buffer = [];
 		    DialogueUtil.makeLine("So this is my studio, what do you think? ", "am", 'talk', 'normal', 0.05, '');
 		    DialogueUtil.makeLine("beep bap bo bop! ", "bf", 'talk', 'normal', 0.05, '');
@@ -25,17 +29,18 @@ function onTimerCompleted(tag, loops, loopsLeft)
 			DialogueUtil.makeLine("Alright then fine! Show me what you got!", "am", 'talk', 'normal', 0.05, '');
 
         ]])
-		sayDialogue('');
-	end
-    clearDialogue();
-end
+playMusic('pause_busridehome', 0.7, true)
+sayDialogue('');
 
+end
+clearDialogue();
+end
 
 -- Dialogue (When a dialogue is finished, it calls startCountdown again)
 function onNextDialogue(count)
-	-- triggered when the next dialogue line starts, 'line' starts with 1
+-- triggered when the next dialogue line starts, 'line' starts with 1
 end
 
 function onSkipDialogue(count)
-	-- triggered when you press Enter and skip a dialogue line that was still being typed, dialogue line starts with 1
+-- triggered when you press Enter and skip a dialogue line that was still being typed, dialogue line starts with 1
 end

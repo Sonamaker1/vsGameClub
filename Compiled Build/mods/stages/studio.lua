1eea68230e9757139aee492895764343f40239da
latterr <98669157+latterr@users.noreply.github.com>
2023-12-19 00:39:56 +0300

    Add bg code

--- Compiled Build/mods/stages/studio.lua
+++ Compiled Build/mods/stages/studio.lua
@@ -2,5 +2,29 @@ function onCreate()
 makeLuaSprite("bg","amstudio",0,0)
 scaleObject('bg', 1.2, 1.2, true)
 addLuaSprite("bg",false)
-close(true)
+
+makeLuaSprite("bg2","BG_GF",1012,425)
+scaleObject('bg2', 0.975, 0.973)
+addLuaSprite("bg2",false)
+
+makeLuaSprite("bg3","FG_GF",1012,430)
+scaleObject('bg3', 0.975, 0.975)
+addLuaSprite("bg3",false)
+end
+
+function onCreatePost()
+setObjectOrder('bg3', getObjectOrder('gfGroup') + 1)
+setObjectOrder('dadGroup', getObjectOrder('bg3') + 1)
+end
+
+
+function onUpdatePost()
+setProperty('bg2.visible', getProperty('bg.visible'))
+setProperty('bg2.alpha', getProperty('bg.alpha'))
+
+setProperty('bg3.visible', getProperty('bg.visible'))
+setProperty('bg3.alpha', getProperty('bg.alpha'))
+
+setProperty('gf.visible', getProperty('bg.visible'))
+setProperty('gf.alpha', getProperty('bg.alpha'))
 end
